70bbcc7c451b8caf848335a22190f39f23ba74b0
Dimitrios Kremmydas <kremmydas.d@gmail.com>
2025-01-06 02:56:19 +0100

    second commit

---
+++ custom.h
@@ -0,0 +1,9 @@
+#define LWIP_HTTPD_CGI 1  // Enable CGI (Common Gateway Interface) support
+#define LWIP_HTTPD_SSI 1
+#define LWIP_HTTPD_SUPPORT_POST 1
+#define LWIP_MDNS_RESPONDER 0
+#define LWIP_HTTPD_SSI_MULTIPART 1
+
+//#define LWIP_HTTPD_CUSTOM_FILES 1
+
+#define HTTPD_FSDATA_FILE "pico_fsdata.inc"
